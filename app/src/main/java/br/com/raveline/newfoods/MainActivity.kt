8a08562a894ae662c7194a6a5a1524c5a82c9cfd
ravelinejunior <junior.raveline@hotmail.com>
2021-08-02 23:21:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/raveline/newfoods/MainActivity.kt
@@ -0,0 +1,42 @@
+package br.com.raveline.newfoods
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import br.com.raveline.newfoods.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+       // navController = findNavController(R.id.nav_graph)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipesFragment_id,
+                R.id.favoritesFragment_id,
+                R.id.jokeFragment_id
+            )
+        )
+
+        binding.bnvMainId.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
